65f068b808f72881b8ec11f59fb1e53c2e41b78b
Michael Waldron <mikewaldron5@gmail.com>
2022-01-10 16:48:22 -0500

    commit tree ab838fe1b64f14e6dd4d90c4180be184f765ffdd

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (block) {
+    if (block >= 42) {
+        return block - 42 
+    } else if (block < 42) {
+        return 42 - block
+    }
+}
+
+function distanceFromHqInFeet(block) {
+    let distance = distanceFromHqInBlocks(block)
+    return (distance * 264)
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (end > start){
+        return (end - start) * 264;
+
+    }else{
+    return (start - end) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let fare = distanceTravelledInFeet(start, destination)
+    if (fare <= 400) {
+        return 0
+    } else if (fare > 400 && fare <= 2000) {
+        return (fare - 400) *.02
+    } else if (fare > 2000 && fare < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
